2523ff7cc78d2cdbedd4c1451372f0d1ad5fd7fc
VBonamassa3 <157747000+VBonamassa3@users.noreply.github.com>
2024-01-30 10:41:17 -0500

    First project from IntelliJ

---
+++ DeclaringVariables.java
@@ -0,0 +1,10 @@
+public class DeclaringVariables {
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 50000.0;
+        String name = "John";
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(name);
+    }
+}
